a38b70626620bff7d94dc920e620661bc021065a
Adam CAVILLON <contact@codinbox.fr>
2023-02-26 19:28:53 +0100

    Added project base

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    java
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "fr.codinbox"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven(url = "https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    implementation("commons-io:commons-io:2.11.0")
+}
+
+val targetJavaVersion = JavaVersion.VERSION_17
+java {
+    sourceCompatibility = targetJavaVersion
+    targetCompatibility = targetJavaVersion
+    if (JavaVersion.current() < targetJavaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion.majorVersion))
+    }
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    if (targetJavaVersion >= JavaVersion.VERSION_1_10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion.majorVersion.toInt()) // The string represent a number, like "1" for Java1
+    }
+    options.encoding = Charsets.UTF_8.name()
+}
+
+tasks.processResources.configure {
+    // Define properties
+    val props = mapOf(Pair("version", version))
+
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
---
+++ src/main/java/fr/codinbox/base/BaseMain.java
@@ -0,0 +1,17 @@
+package fr.codinbox.base;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BaseMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
