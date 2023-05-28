a7ca5636524fcdd019f021057c8e321e6ed4be01
Adam CAVILLON <contact@codinbox.fr>
2023-05-28 20:21:42 +0200

    Added javadoc for NpcClickedListener

--- api/src/main/java/fr/codinbox/npclib/api/npc/event/NpcClickedListener.java
+++ api/src/main/java/fr/codinbox/npclib/api/npc/event/NpcClickedListener.java
@@ -2,8 +2,16 @@ package fr.codinbox.npclib.api.npc.event;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A listener for npc clicked events.
+ */
 public interface NpcClickedListener {
 
+    /**
+     * Called when a npc is clicked.
+     *
+     * @param event the event
+     */
     void onNpcClicked(@NotNull NpcClickedEvent event);
 
 }
