b86a06997e1d56d541698cdb9aeaf94e07928327
Adam CAVILLON <contact@codinbox.fr>
2023-05-28 20:26:57 +0200

    Added Skin javadoc + Added TODOs

--- api/src/main/java/fr/codinbox/npclib/api/npc/skin/Skin.java
+++ api/src/main/java/fr/codinbox/npclib/api/npc/skin/Skin.java
@@ -4,10 +4,21 @@ import org.jetbrains.annotations.NotNull;
 
 public interface Skin {
 
+    /**
+     * Create a new skin from a value and a signature.
+     *
+     * @param value the value
+     * @param signature the signature
+     * @return the skin
+     */
     @NotNull
     static ValueSignatureSkin fromValueAndSignature(@NotNull String value, @NotNull String signature) {
         return new ValueSignatureSkin(value, signature);
     }
+
+    // TODO: Add a method to create a skin from a player name.
+    // TODO: Add a method to create a skin from a player UUID.
+    // TODO: Add a method to create a skin from a skin URL.
 
     /**
      * Get the skin value.
