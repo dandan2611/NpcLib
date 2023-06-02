c1fd78e961d240b9869a8d5194256c92b8bf50ac
Adam CAVILLON <contact@codinbox.fr>
2023-06-02 19:04:22 +0200

    Add Skin defaultSkin method

--- api/src/main/java/fr/codinbox/npclib/api/npc/skin/Skin.java
+++ api/src/main/java/fr/codinbox/npclib/api/npc/skin/Skin.java
@@ -12,9 +12,19 @@ public interface Skin {
      * @param signature the signature. Can be null (Notchian client does not seem to care about it).
      * @return the skin
      */
-    @NotNull
-    static ValueSignatureSkin fromValueAndSignature(@NotNull String value, @Nullable String signature) {
+    static @NotNull ValueSignatureSkin fromValueAndSignature(@NotNull String value, @Nullable String signature) {
         return new ValueSignatureSkin(value, signature);
+    }
+
+    /**
+     * A default skin.
+     * <br>
+     * Skin is calculated from the player name and default textures.
+     *
+     * @return the default skin
+     */
+    static @Nullable ValueSignatureSkin defaultSkin() {
+        return null;
     }
 
     // TODO: Add a method to create a skin from a player name.
