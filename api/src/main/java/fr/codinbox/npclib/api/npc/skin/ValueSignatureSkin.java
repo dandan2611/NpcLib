2d4782bed652b8d791f4e01a8b254302e192dc26
Adam CAVILLON <contact@codinbox.fr>
2023-06-06 09:44:06 +0200

    Add ValueSignatureSkin javadoc + Add missing annotations

--- api/src/main/java/fr/codinbox/npclib/api/npc/skin/ValueSignatureSkin.java
+++ api/src/main/java/fr/codinbox/npclib/api/npc/skin/ValueSignatureSkin.java
@@ -3,23 +3,42 @@ package fr.codinbox.npclib.api.npc.skin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a Skin with a value and a signature.
+ */
 public class ValueSignatureSkin implements Skin {
 
     private final @NotNull String value;
     private final @Nullable String signature;
 
+    /**
+     * Create a new skin from a value and a signature.
+     *
+     * @param value the value
+     * @param signature the signature. Can be null (Notchian client does not seem to care about it).
+     */
     protected ValueSignatureSkin(@NotNull String value, @Nullable String signature) {
         this.value = value;
         this.signature = signature;
     }
 
+    /**
+     * Get the skin value.
+     *
+     * @return the skin value
+     */
     @Override
-    public String getValue() {
+    public @NotNull String getValue() {
         return this.value;
     }
 
+    /**
+     * Get the skin signature.
+     *
+     * @return the skin signature
+     */
     @Override
-    public String getSignature() {
+    public @Nullable String getSignature() {
         return this.signature;
     }
 
