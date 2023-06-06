0df285bb89855b24107be3f9abb539ad2f5f97e5
Adam CAVILLON <contact@codinbox.fr>
2023-06-06 09:34:42 +0200

    Add NpcRenderLogic javadoc

--- api/src/main/java/fr/codinbox/npclib/api/npc/viewer/NpcRenderLogic.java
+++ api/src/main/java/fr/codinbox/npclib/api/npc/viewer/NpcRenderLogic.java
@@ -5,8 +5,21 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a NPC render logic.
+ * <br>
+ * A render logic is used to determine whether a NPC should be rendered or not.
+ */
 public interface NpcRenderLogic {
 
+    /**
+     * Check if the NPC should be rendered.
+     *
+     * @param npc the NPC
+     * @param player the player
+     * @param viewer the viewer
+     * @return {@code true} if the NPC should be rendered, {@code false} otherwise
+     */
     boolean shouldBeRendered(@NotNull Npc npc, @NotNull Player player, @Nullable NpcViewer viewer);
 
 }
